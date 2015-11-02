a2b0137c590def67e43cf2c615aaf19ba4287138
Bradley Stafford <Morklympious@gmail.com>
2015-11-02 13:31:41 -0800

    Update entry.js
    Clarify wording

--- src/entry.js
+++ src/entry.js
@@ -3,10 +3,13 @@
   This is it. The big one, the workhorse, the module that tirelessly
   shoulders the burden. 
 
-  Ideally, you'll be requiring all of your other login in here to either
+  Ideally, you'll be requiring all of your other modules in here to either
     a) Setup the initial stuff you're gonna need
     b) Gather all the stuff you need in the same place to do stuff with it.
+    
+  Notes:
+  
+  - If you want to put something on the window object, you'll be referencing 'global' to
+    do so! (ex. global.namespace === window.namespace) in a browser, 'global' becomes 'window'
 */
 
-var behavior = require('./js/behavior.js')
-global.behavior = behavior
